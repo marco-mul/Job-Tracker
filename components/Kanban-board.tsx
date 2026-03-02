53ad170f341f070f114e42f88039154e46beb507
Marco Muller <marco.muller.mm@gmail.com>
2026-03-02 13:40:13 +0100

    resize board

--- components/Kanban-board.tsx
+++ components/Kanban-board.tsx
@@ -98,7 +98,7 @@ function DroppableColumn({
     column.jobApplications?.sort((a, b) => a.order - b.order) || [];
 
   return (
-    <Card className="min-w-[300px] flex-shrink-1 shadow-md p-0">
+    <Card className="min-w-[400px] flex-shrink-1 shadow-md p-0">
       <CardHeader
         className={`${config.color} text-white rounded-t-lg pb-3 pt-3`}
       >
